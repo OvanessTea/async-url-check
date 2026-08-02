f4004e04bf963148cb9d8ddd1a113a5345898ba8
OvanessTea <OvanessTea@yandex.com>
2026-08-02 17:01:07 +0300

    add server cors

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,12 +1,19 @@
 import express, {Request, Response} from 'express';
-
+import cors from 'cors';
+import dotenv from 'dotenv';
 import { jobsRouter } from './modules/jobs/jobs.routes';
 import { errorHandler } from './common/errors/error-handler';
 
 export const app = express();
-
+dotenv.config();
 app.use(express.json());
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    optionsSuccessStatus: 200,
+    credentials: true,
+};
 
+app.use(cors(corsOptions));
 app.get('/health', (_req: Request, res: Response) => {
     res.json({
         status: 'ok'
