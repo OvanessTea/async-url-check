27a9434d497c13eaea51a5aa3c04c692246e4937
OvanessTea <OvanessTea@yandex.com>
2026-08-02 17:07:51 +0300

    add simple UI to check client & api working

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
+import { useEffect } from 'react';
+import { getJobs } from '@/entities/job/api/jobs-api';
+import { useJobsStore } from '@/entities/job/model/store';
 
 function App() {
+	const jobs = useJobsStore((state) => state.jobs);
+	const setJobs = useJobsStore((state) => state.setJobs);
 
-  return (
-    <>
-      <div>HELLO!</div>
-    </>
-  )
+	useEffect(() => {
+		getJobs()
+			.then(setJobs)
+			.catch(console.error);
+	}, [setJobs])
+
+	return (
+		<div>
+			<h1>Job Checker</h1>
+			<h2>Jobs</h2>
+			{jobs.map((job) => (
+				<div key={job.id}>
+					<strong>{job.id}</strong>
+					<div>Status: {job.status}</div>
+					<div>URLs: {job.urlCount}</div>
+				</div>
+			))}
+		</div>
+	)
 }
 
 export default App
